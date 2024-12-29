8742f29e45f68a6d082d4876ddf8f771b62c30f4
rangamach <117797904+rangamach@users.noreply.github.com>
2024-12-29 11:30:00 +0530

    Added struct leveldata and enum blocktype

---
+++ Array-Jumper/header/Level/BlockType.h
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace Level
+{
+    enum class BlockType
+    {
+        Obstacle_One = -1,
+        Obstacle_Two = -2,
+        Target = 0,
+        One = 1,
+        Two = 2,
+        Three = 3,
+    };
+}
+
